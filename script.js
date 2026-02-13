752ceaf62a05a28dd73f5a33373bd5633d9d3480
gonzalomerlo1997x-hue <gonzalomerlo1997x@gmail.com>
2026-02-13 10:32:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// FECHA DE INICIO
+const inicio = new Date("2023-09-12T16:23:00");
+
+function actualizarTiempo() {
+  const ahora = new Date();
+  const diff = ahora - inicio;
+
+  const dias = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const horas = Math.floor(diff / (1000 * 60 * 60)) % 24;
+  const minutos = Math.floor(diff / (1000 * 60)) % 60;
+  const segundos = Math.floor(diff / 1000) % 60;
+
+  document.getElementById("tiempo").innerText =
+    `${dias} días ${horas} horas ${minutos} minutos ${segundos} segundos`;
+}
+
+setInterval(actualizarTiempo, 1000);
+actualizarTiempo();
+
+document.getElementById("play").onclick = () => {
+  document.getElementById("mapa").style.display = "block";
+  document.getElementById("arbol").style.display = "block";
+
+  document.getElementById("mapa").innerHTML = `
+    <iframe
+      width="100%"
+      height="300"
+      style="border:0"
+      loading="lazy"
+      src="https://maps.google.com/maps?q=-32.940241,-60.672443&z=17&output=embed">
+    </iframe>
+  `;
+};
